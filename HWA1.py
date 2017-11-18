7d92409c31ebdddc07d1ac530c9b293a0260fe80
Ewoud Samuels <jinior@live.nl>
2017-11-18 13:47:49 +0100

    plotted everything

--- HWA1.py
+++ HWA1.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 csvpath = "speed_dating_assignment.csv"
 dataset = pd.read_csv(csvpath)
 
 dataset.head()
+plt.matshow(dataset.corr())
+plt.show()
