84be036734c129bfe4bc4e4f5cebdb4ebce1e8c8
Ewoud Samuels <jinior@live.nl>
2017-11-22 14:18:30 +0100

    working dicision tree
    also cleaned up code especially for Thijs

--- HWA1/Question1.py
+++ HWA1/Question1.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sklearn as sk
 from sklearn import tree
 import numpy as np
-
 import seaborn as sns
+
 csvpath = "speed_dating_assignment.csv"
 df_import = pd.read_csv(csvpath)
 df_import
 df_import.head()
-
 
 ###############QUESTION1.1############################
 
@@ -20,19 +20,8 @@ df_partners = df_partners.drop_duplicates()
 df = pd.merge(df_import, df_partners, on=[
     'pid'], how='left')
 
-# filter relevant columns
-df_filtered = df_import[['iid', 'age', 'pid', 'dec']]
-
-# generate parter ages
-#df_partners = df_filtered.copy()
-#df_partners = df_import[['iid', 'age']]
-#df_partners.columns = ['pid', 'partner_age']
-#df_partners = df_partners.drop_duplicates()
-df = pd.merge(df_import, df_partners, on=[
-    'pid'], how='left')
-df_ageMatrix = pd.merge(df_filtered, df_partners, on=[
-    'pid'], how='left')
-# end of generating partner ages
+# generate age matrix
+df_ageMatrix = df[['iid', 'age', 'pid', 'dec', 'partner_age']]
 
 # build a matrix with age (rows) and partner age (columns); decision rate is shown in cells
 matrix = pd.crosstab(df_ageMatrix['age'], df_ageMatrix['partner_age'],
@@ -50,19 +39,11 @@ cm = sns.light_palette("orange", as_cmap=True)
 # Sample containing 80% of females
 
 femaleTrain = df[df['gender'] == 0].sample(frac=0.8)
-
-femaleTest = df.drop(femaleTrain.index)
-
-# Get rid of gender column
-del femaleTrain["gender"]
-
+femaleFilter = df.drop(femaleTrain.index)
 
 # Sample containing 80% of males
 maleTrain = df[df["gender"] == 1].sample(frac=0.8)
-
 maleTest = df.drop(maleTrain.index)
-
-del maleTrain["gender"]
 
 # We start building the tree by defining target and predictor features
 target = femaleTrain["dec"]
@@ -76,6 +57,7 @@ femaleTrain['round'].fillna(femaleTrain['age'].median(), inplace=True)
 
 features = femaleTrain[["age", "round"]].values
 
+
 femaleTrain.describe()
 # Here is where we will controll things such as overfiting
 test_tree = tree.DecisionTreeClassifier()
@@ -85,8 +67,21 @@ test_tree = test_tree.fit(features, target)
 femaleTest['age'].dropna(inplace=True)
 femaleTest['round'].dropna(inplace=True)
 
-test_tree.predict(femaleTest)
+testForTree = femaleTest.filter(items=['age', 'round'])
 
-femaleTest
+testForTree['age'].fillna(value=20, inplace=True)
+testForTree['round'].fillna(value=20, inplace=True)
 
-#
+testForTree['age'].median()
+
+testForTree
+
+testForTree.isna().values.any()
+predictions = test_tree.predict(testForTree)
+
+sk.metrics.accuracy_score(femaleTest['dec'], predictions, normalize=True)
+
+comparison = femaleTest['dec']
+
+
+predictions
